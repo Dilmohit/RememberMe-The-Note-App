1691e4bdc503771d83dc96c6deb69fe3c3a92b64
Dilmohit <dilmohitwork212@gmail.com>
2022-06-11 20:10:02 +0530

    Update Gradle current version 7.1.2 to version 7.2.1.

--- app/src/main/java/com/example/rememberme/NewNotesActivity.java
+++ app/src/main/java/com/example/rememberme/NewNotesActivity.java
@@ -34,6 +34,11 @@ public class NewNotesActivity extends AppCompatActivity {
             notesEdttxt.setText(intent.getStringExtra(EXTRA_NOTE));
         }
 
+//        Intent in = getIntent();
+//        String easyPuzzle = in.getExtras().getString("epuzzle");
+
+
+
         // adding on click listener for our save button.
         notesBtn.setOnClickListener(v -> {
             // getting text value from edittext and validating if
